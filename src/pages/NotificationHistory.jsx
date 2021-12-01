0fc332fc05dd55536c4d20bddd44c9020ab78ee7
anderson-santos-sidia <anderson.santos@sidia.com>
2021-12-01 11:33:18 -0300

    paginacao inicial

--- src/pages/NotificationHistory.jsx
+++ src/pages/NotificationHistory.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import {
   List,
   ListItem,
+  Stack,
+  Button,
+  Text,
 } from "@chakra-ui/react";
 import {api} from '../services/api'
 import HistoryItem from '../components/HistoryItem'
@@ -17,18 +20,28 @@ export default function NotificationHistory (){
     : process.env.REACT_APP_HOM_URL;
 
   const [historyList, setHistoryList] = React.useState(null);
+  const [page, setPage] = React.useState(1);
   const regPhoto = baseURL+"/photo/reg/thumb/"+user;
   
   
 
   async function getHistoryList(id) {
-    const response = await api.get("/checkin/list/"+id+"/page/1/size/20");
+    const response = await api.get("/checkin/list/"+id+"/page/"+page+"/size/5");
     setHistoryList(response.data);
+  }
+
+  function alterPage(cmd){
+    if(cmd==='next'){
+      if(historyList.length>1)
+        setPage(page+1)
+    }else{
+      setPage(page-1)
+    }
   }
 
   useEffect(() => {
     getHistoryList(user);
-  }, []
+  }, [page]
   )
 
   if (!historyList) return (
@@ -45,7 +58,7 @@ export default function NotificationHistory (){
       </List>
     </>
   )
-//process.env.REACT_APP_HOM_URL+"/photo/reg/thumb/"+item.userId
+
   return (
     <>
       <List spacing={5} margin="2rem">
@@ -60,6 +73,50 @@ export default function NotificationHistory (){
           </ListItem>
         ))}
       </List>
+      <PageNav/>
     </>
   );
+
+
+  function PageNav(){
+    return(
+      <Stack
+      direction={"row"}
+      align="center"
+      justify="center"
+      spacing={6}
+    >
+      <Button
+        onClick={()=>{if(page>1)alterPage('prev')}}
+        isActive={page===1?true:false}
+        display={{ base: "inline-flex", md: "inline-flex" }}
+        fontSize={"sm"}
+        fontWeight={600}
+        color={"white"}
+        bg={"blue.500"}
+        _hover={{
+          bg: "gray.500",
+        }}
+      >
+        &lt;
+      </Button>
+      <Text>{page}</Text>
+      <Button
+        onClick={()=>alterPage('next')}
+        display={{ base: "inline-flex", md: "inline-flex" }}
+        fontSize={"sm"}
+        fontWeight={600}
+        color={"white"}
+        bg={"blue.500"}
+        _hover={{
+          bg: "gray.500",
+        }}
+      >
+        &gt;
+      </Button>
+    </Stack>
+    )
+  }
 };
+
+
