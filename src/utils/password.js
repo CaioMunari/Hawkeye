0ffb73717901f9763f412c4f144d18b7c8374abb
anderson-santos-sidia <anderson.santos@sidia.com>
2021-12-03 10:08:43 -0300

    HotFix: password extra char assurance

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,2 +1,11 @@
 export const generatePassword = (word) =>
-  btoa(word) + Math.random().toString(36).substring(3, 16);
+  btoa(word) + random10();
+
+  
+function random10(){
+  let a = ""
+  for(let y=0;y<5;y++){
+      a = a + Math.random().toString(36).substring(3, 16)
+  }
+  return a.slice(0,10) 
+}
