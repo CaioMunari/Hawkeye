05a5461e067787fa1126b9ba877c027a8cd16c3f
anderson-santos-sidia <anderson.santos@sidia.com>
2021-12-03 12:09:11 -0300

    timestamp

---
+++ src/utils/time.js
@@ -0,0 +1,18 @@
+export const timeStamp = () => {
+    function zeroAdd(dt) {
+      if(dt>10){
+        return dt.toString()
+      }else{
+        return "0"+dt.toString()
+      }
+    }
+    const date = new Date()
+    let frag = {
+      'YYYY': date.getFullYear().toString(),
+      'MM': zeroAdd(date.getMonth()+1),
+      'DD': zeroAdd(date.getDate()),
+      'hh': zeroAdd(date.getHours()),
+      'mm': zeroAdd(date.getMinutes())
+    }
+    return frag.YYYY+frag.MM+frag.DD+frag.hh+frag.mm
+  }
