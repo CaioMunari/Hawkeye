fb9f6db748efa8f4cc0ae93bcf9343a5630a65dd
Rafael Barbosa de Carvalho <rafael.carvalho@sidia.com>
2021-12-28 09:25:31 -0400

    Added serial number screen

---
+++ src/pages/SerialNumber.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Flex, Stack, Text } from "@chakra-ui/react";
+import { getSNToken, setSNToken } from "../services/auth";
+import { useNavigate } from "react-router-dom";
+import { Heading } from "@chakra-ui/react";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import useOrientation from "../hooks/useOrientation";
+
+const SerialNumber = () => {
+  const navigate = useNavigate();
+  const [serialNumber, setSerialNumber] = useState(getSNToken());
+  const [error, setError] = useState(false);
+
+  const { getOrientationValue } = useOrientation();
+
+  const registerSerialNumber = (e) => {
+    e.preventDefault();
+    if (!serialNumber || serialNumber.length < 5) {
+      setError(true);
+      return;
+    }
+    setSNToken(serialNumber);
+    navigate("/login");
+  };
+
+  return (
+    <Flex
+      width="100%"
+      h="100%"
+      align="center"
+      justify={getOrientationValue("flex-end", "center")}
+      px={{ base: "0", md: getOrientationValue("5rem", "0") }}
+    >
+      <form style={{ width: "30%" }} onSubmit={registerSerialNumber}>
+        <Stack
+          direction="column"
+          minWidth={{ base: "100vw", md: getOrientationValue("20vw", "80vw") }}
+          bg="white"
+          height={{ base: "100vh", md: "auto" }}
+          p={12}
+          justify={{ base: "flex-start", md: "flex-start" }}
+          borderRadius={{ base: 0, md: 12 }}
+        >
+          <Heading w="50%" fontWeight="normal" style={{ marginBottom: 30 }}>
+            Registro do dispositivo
+          </Heading>
+          <Heading
+            fontSize="1.5rem"
+            fontWeight="normal"
+            style={{ marginBottom: 30 }}
+          >
+            Antes de começar registre o serial number deste dispositivo. É
+            simples e rápido.
+          </Heading>
+          <Input
+            bg="white"
+            placeholder="Serial Number"
+            value={serialNumber}
+            onChange={(e) => setSerialNumber(e.target.value)}
+            isInvalid={error}
+            errorBorderColor="red.500"
+            color={error && "red"}
+          />
+          {error && (
+            <Text
+              color="red"
+              fontSize="0.9rem"
+              style={{ marginTop: 10 }}
+              textAlign="center"
+              width="50%"
+            >
+              Digite um número de dispositivo correto!
+            </Text>
+          )}
+          <Button
+            onClick={registerSerialNumber}
+            colorScheme="teal"
+            backgroundColor="teal.300"
+            type="submit"
+            style={{ marginTop: 25, textTransform: "uppercase" }}
+          >
+            Registrar
+          </Button>
+        </Stack>
+      </form>
+    </Flex>
+  );
+};
+
+export default SerialNumber;
