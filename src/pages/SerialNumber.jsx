b866ab46a61a0403b7a6ac1719cfa7b195f9d1e7
Rafael Barbosa de Carvalho <rafael.carvalho@sidia.com>
2022-01-05 12:59:45 -0400

    Experiment

--- src/pages/SerialNumber.jsx
+++ src/pages/SerialNumber.jsx
@@ -18,18 +18,28 @@ const SerialNumber = () => {
     Notification.requestPermission().then(function (permission) {
       if (permission === "granted") {
         setInterval(() => {
-          updateList();
+          const greeting = new Notification("Hi, How are you?", {
+            body: "Have a good day",
+            icon: "./images/logo.svg",
+          });
+
+          // navigate to the https://www.javascripttutorial.net/ on click
+          greeting.addEventListener("click", function () {
+            window.open(
+              "https://www.javascripttutorial.net/web-apis/javascript-notification/"
+            );
+          });
         }, 5000);
       } else console.error("Permission was not granted.");
     });
   });
 
-  const updateList = () => {
-    // setInterval(() => {
-    const newList = [...list];
-    setList([...newList, Math.random()]);
-    // }, 5000);
-  };
+  // const updateList = () => {
+  //   // setInterval(() => {
+  //   const newList = [...list];
+  //   setList([...newList, Math.random()]);
+  //   // }, 5000);
+  // };
 
   const { getOrientationValue } = useOrientation();
 
